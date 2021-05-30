32e96a9092cec8359a7c1bdb97dfd39ca5ca4406
emae1712 <melissa.aechegaray@gmail.com>
2021-05-30 03:17:03 -0500

    | WIP | welcome to the Pokemon game
    creation of some functions that show images and some clues of the pokemon to guess

--- FinalProject.py
+++ FinalProject.py
@@ -3,6 +3,7 @@ import requests
 from io import BytesIO
 
 import json
+import random
 
 def shadow(filename):
     response = requests.get(filename)
@@ -10,21 +11,48 @@ def shadow(filename):
     image = ImageOps.colorize(img,black="white", white="black")
     return image
 
+def movements(quick_move):
+    value =[]
+    for i in range(len(quick_move)):
+            value.append(quick_move[i]['name'])
+    return value
+
 def main():
-    print('Who is that Pokemon? ')
+    print('👋 Welcome to Pokemon data')
+    print('Below is information for 251 Pokemon from the Kanto and Johto regions. Your mission will be to guess the Pokemon shown in the image. \nGotta catch \'em all!\n')
+    print('🤩 Who is that Pokemon? ')
+    input("Press enter to see the image ")
+
+    index = random.randint(0,250)
 
     with open('pokemon.json') as Pokemon:
         data = json.load(Pokemon)
-        img_url=data['pokemon'][3]['img']
+        img_url=data['pokemon'][index]['img']
         response = requests.get(img_url)
         img = Image.open(BytesIO(response.content))
-        img.show()
-        print(img)
 
         #Grayscale image
         img_shadow = shadow(img_url)
         img_shadow.show()
-        print(response)
+
+        #Clues
+        pokemon_type = data['pokemon'][index]['type']
+        region = data['pokemon'][index]['generation']['name']
+        quick_move = movements(data['pokemon'][index]['quick-move'])
+        
+        name =data['pokemon'][index]['name']
+        print('\n💡 Some clues about this pokemon')
+        print("Type: ", pokemon_type)
+        print("Region: ", region)
+        print('Quick move: ', quick_move)
+        print('name ', name)
+
+        #Attempts
+        guess = len(name)*'-'
+        print('The Pokemon name now looks like this: ',guess)
+       
+
+        img.show()
 
 if __name__== '__main__':
     main()
